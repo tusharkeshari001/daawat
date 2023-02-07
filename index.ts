a5294d4efbdb2e0e389363e543066a53a5c4928f
Tushar.k <Tushar.k@kagamierp.com>
2023-02-07 17:40:58 +0530

    #0020: adding chages for trigger

---
+++ index.ts
@@ -0,0 +1,3 @@
+export const handleTrigger = () => {
+    console.log('Click me')
+}
--- index.tsx
+++ index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { handleTrigger } from ".";
 
 const index = () => {
   const [first, setfirst] = React.useState(true);
-  return first ? <div>Welcome to github</div> : <div>Thank you github</div>;
+  return first ? (<div>
+    Welcome to github
+    <button onClick={handleTrigger}></button>
+    </div>) : <div>Thank you github</div>;
 };
 
 export default index;
